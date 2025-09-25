ea6186658aa65d98bfff3dfd229ec3b868955a2d
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-09-25 16:42:00 -0700

    S37 141 The Parsing Phase and Syntax Errors

---
+++ calorie.py
@@ -0,0 +1,25 @@
+print("Program has started".make_capital())
+print(x)
+
+class Calorie:
+
+    def __init__(self, weight, height, age temperature):
+        self.weight = weight
+        self.height = height
+        self.age = age
+        self.temperature = temperature
+
+    def calculate(self):
+        result = 10 * self.weight + 6.5 * self.height + 5 * age - 5 - self.temperature * 10
+        return result
+
+    def get_kj(self):
+        kcal = self.calculate()
+        return kcal * 4.184
+
+    def kj_to_file(self):
+        filepath = "calories.txt"
+        with open(filepath, 'w') as file: file.write(self.get_kj())
+
+calorie = Calorie(175, 70, 33, 30)
+calories = carolie.kj_to_file())
