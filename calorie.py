56d1546019f826735491c63f2598372c3af60073
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-09-25 17:02:44 -0700

    S37 143 Traceback with multiple errors and fix it.

--- calorie.py
+++ calorie.py
@@ -9,7 +9,7 @@ class Calorie:
         self.temperature = temperature
 
     def calculate(self):
-        result = 10 * self.weight + 6.5 * self.height + 5 * age - 5 - self.temperature * 10
+        result = 10 * self.weight + 6.5 * self.height + 5 * self.age - 5 - self.temperature * 10
         return result
 
     def get_kj(self):
@@ -19,7 +19,6 @@ class Calorie:
     def kj_to_file(self):
         filepath = "calories.txt"
         with open(filepath, 'w') as file: file.write(self.get_kj())
-
 
 
 calorie = Calorie(175, 70, 33, 30)
